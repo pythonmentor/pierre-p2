2967b5a37e3b8c678d7329f85663cf9cce6f8afa
Pierre Rondeau <pierre.rondeau9pro@gmail.com>
2021-04-15 13:34:20 +0200

    ajout fonction description + paramètre argument

--- script.py
+++ script.py
@@ -3,9 +3,15 @@ from bs4 import BeautifulSoup
 
 
 def scraping_book():
-  url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
-  response = requests.get(url)
-  if response.ok:
-      soup = BeautifulSoup(response.content, "html.parser")
-      title = soup.find("div", {"class": "col-sm-6 product_main"}).find("h1")
-      print(title.text)
+    url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+    response = requests.get(url)
+    if response.ok:
+        soup = BeautifulSoup(response.content, "html.parser")
+        title = soup.select_one('.product_main h1')      # remplace find("div", {"class": "col-sm-6 product_main"}).find("h1")
+        print(title.text)
+        product_description(soup)
+
+      
+def product_description(soup):
+  description = soup.select_one('.sub-header ~ p')
+  print(description.text)
