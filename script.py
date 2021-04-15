ab79c77be6ec1aecbc77d32200c146a3094f0d5b
Pierre Rondeau <pierre.rondeau9pro@gmail.com>
2021-04-15 11:09:49 +0200

    premier commit du script.py

---
+++ script.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup 
+
+
+def scraping_book():
+  url = "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+  response = requests.get(url)
+  if response.ok:
+      soup = BeautifulSoup(response.content, "html.parser")
+      title = soup.find("div", {"class": "col-sm-6 product_main"}).find("h1")
+      print(title.text)
