bf54c98422d70deb6391c5587ab0c9f546bfa7be
Pierre Rondeau <pierre.rondeau9pro@gmail.com>
2021-04-15 13:36:25 +0200

    ajout return 18 12

--- script.py
+++ script.py
@@ -9,9 +9,10 @@ def scraping_book():
         soup = BeautifulSoup(response.content, "html.parser")
         title = soup.select_one('.product_main h1')      # remplace find("div", {"class": "col-sm-6 product_main"}).find("h1")
         print(title.text)
-        product_description(soup)
+        description = product_description(soup)
 
       
 def product_description(soup):
   description = soup.select_one('.sub-header ~ p')
   print(description.text)
+  return description
