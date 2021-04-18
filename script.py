7626154a81c17ec559b92a6ebdf272a37cc6d502
Pierre Rondeau <pierre.rondeau9pro@gmail.com>
2021-04-18 12:24:38 +0200

    ajout de la fonction upc avec tableau complet donc à modif

--- script.py
+++ script.py
@@ -7,12 +7,24 @@ def scraping_book():
     response = requests.get(url)
     if response.ok:
         soup = BeautifulSoup(response.content, "html.parser")
-        title = soup.select_one('.product_main h1')      # remplace find("div", {"class": "col-sm-6 product_main"}).find("h1")
-        print(title.text)
+        title = soup.select_one('.product_main h1').text      # remplace find("div", {"class": "col-sm-6 product_main"}).find("h1")
+        print(title)
         description = product_description(soup)
+        category = product_category(soup)
+        upc = product_upc(soup)
 
-      
+     
 def product_description(soup):
-  description = soup.select_one('.sub-header ~ p')
-  print(description.text)
+  description = soup.select_one('.sub-header ~ p').text
+  print(description)
   return description
+
+def product_category(soup):
+  category = soup.select('.breadcrumb li')[-2].text
+  print(category)
+  return category
+
+def product_upc(soup):
+  upc = soup.select('table',{'table-striped' : 'td'})
+  print(upc)
+  return upc
